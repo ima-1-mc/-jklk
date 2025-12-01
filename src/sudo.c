773eb610267642f17715b2ed5f302be148befdda
Ima ASSONGBA <ima.assongba@epitech.eu>
2025-12-01 17:31:11 +0100

    sudo

--- src/sudo.c
+++ src/sudo.c
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2025
 ** exec
 ** File description:
-** exec                                                                                                                               
+** exec
 */
 
 #include "../include/my.h"
