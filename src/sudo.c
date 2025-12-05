17ab35331dade3fdab8ea4194abe2ed255ee6207
Ima ASSONGBA <ima.assongba@epitech.eu>
2025-12-05 11:21:42 +0100

    sudo

--- src/sudo.c
+++ src/sudo.c
@@ -9,13 +9,14 @@
 
 int main(int ac, char **av)
 {
-    char *const tab[] = {av[1], av[2], av[3], av[4], av[5], NULL};
-
-    if (ac == 1)
+   if (ac < 2)
         return 84;
-    if (strcmp(av[1], "-h") == 0)
+
+   if (ac == 2 && strcmp(av[1], "-h") == 0) {
         usage();
-    if (execvp(av[1], tab) == -1)
+        return 0;
+    }
+   if (execvp(av[1], &av[1]) == -1)
         return 84;
     return 0;
 }
