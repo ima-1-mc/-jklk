7c16e6f7632d7d1cbd17da4c3858b4628b574223
Ima ASSONGBA <ima.assongba@epitech.eu>
2025-12-05 15:04:48 +0100

    sudo

--- src/sudo.c
+++ src/sudo.c
@@ -9,14 +9,13 @@
 
 int main(int ac, char **av)
 {
-   if (ac < 2)
+    if (ac < 2)
         return 84;
-
-   if (ac == 2 && strcmp(av[1], "-h") == 0) {
+    if (ac == 2 && strcmp(av[1], "-h") == 0) {
         usage();
         return 0;
     }
-   if (execvp(av[1], &av[1]) == -1)
+    if (execvp(av[1], &av[1]) == -1)
         return 84;
     return 0;
 }
